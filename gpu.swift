929ebc15ed0618f2cbbd0b1c679439bede67076b
Tomasz Kolinko <kolinko@gmail.com>
2024-03-13 15:45:15 +0100

    cleanup

--- gpu.swift
+++ gpu.swift
@@ -104,30 +104,3 @@ class Gpu {
         
     }
 }
-/*
-func deploy(_ encoder: MTLComputeCommandEncoder, fname: String, buffers: [Bufferable], ints: [Int] = [], threadCount: Int) {
-    if (!globalStates.keys.contains(fname)) {
-            let internalFunc = library.makeFunction(name: fname)!
-            globalStates[fname] = try! device.makeComputePipelineState(function: internalFunc)
-            print("warn:Compute pipeline state for \(fname) not found.")
-        }
-    
-    let internalState = globalStates[fname]!
-        
-    let gridSize = MTLSize(width: threadCount, height: 1, depth: 1)
-    let threadGroupSize = MTLSize(width: internalState.threadExecutionWidth, height: 1, depth: 1)
-
-    encoder.setComputePipelineState(internalState)
-
-    for i in 0..<buffers.count {
-        encoder.setBuffer(buffers[i].buffer, offset: buffers[i].offset, index: i)
-    }
-
-    for i in 0..<ints.count {
-        var x: Int = ints[i]
-        encoder.setBytes(&x, length: MemoryLayout<Int>.stride, index: i+buffers.count)
-    }
-
-    encoder.dispatchThreads(gridSize, threadsPerThreadgroup: threadGroupSize)
-}
-*/
