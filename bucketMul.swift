f5ec2754779abda071ff0254e57087f04de9f4ba
Tomasz Kolinko <kolinko@gmail.com>
2024-04-12 08:38:08 +0200

    refactor

--- bucketMul.swift
+++ bucketMul.swift
@@ -3,21 +3,9 @@ private let prevSize = ScalarFloat(value: 0)
 
 func bucketMulFast(v: VectorFloat, by: ExpertWeights, expNo: ScalarFloat, out: VectorFloat, quant: Double = 0.25) {
     if goNoMuls {return;}
-  //  out.zero()
     let bm = BucketMulFast.shared
-   // bm.dispatch.zero()
-    bm.calcDispatch(v: v, eWeights: by, expNo: expNo, quant: quant)
-    gpu.eval()   
-    print(bm.dispatch.size.long)
-//    print(bm.cutoff.val)
-    gpu.deploy("roundUp", buffers:[bm.dispatch.size, prevSize], ints:[2048], threadCount: 1) // tofix
-    gpu.deploy("zeroRange32", buffers: [bm.dispatch, prevSize, bm.dispatch.size], threadCount: 2048 )
-    bm.mul(by: by, out: out)
-//    if out.hasNan {
-//        print("hello")
-//    }
+    bm.fullMul(v: v, ew: by, expNo: expNo, out: out, quant: quant)
 }
-
 
 class BucketMulFast {
     let probesCount = 4096
@@ -39,12 +27,9 @@ class BucketMulFast {
         assert(ew.probes.cols == 4096, "probes implemented for 4096 only. needs review of sort as well as probeShort")
 
         dispatch.size.zero()
-        
         let q = Int(Double(probesCount-1)*(1-quant))
 
-        gpu.startCapture()
         gpu.deploy("findCutoff32", buffers: [v, ew.probes, expNo, cutoff], ints:[q], threadCount: 1024, threadGroupSize: [1024, 1, 1])
-        gpu.stopCapture()
         
         let chunkSize = 4//w.stats.rows//16
         gpu.deploy("prepareExpertDispatchFast", buffers:[v, ew.stats, expNo, cutoff, dispatch, dispatch.size],
@@ -54,6 +39,16 @@ class BucketMulFast {
     
     private let mulGroups = 32
     private let tmpMulVec = MatrixFloat(shape:[32, 16384])
+
+    func fullMul(v: VectorFloat, ew: ExpertWeights, expNo: ScalarFloat, out: VectorFloat, quant: Double) {
+        calcDispatch(v: v, eWeights: ew, expNo: expNo, quant: quant)
+        
+        gpu.deploy("roundUp", buffers:[dispatch.size, prevSize], ints:[2048], threadCount: 1) // tofix
+        gpu.deploy("zeroRange32", buffers: [dispatch, prevSize, dispatch.size], threadCount: 2048 )
+        
+        mul(by: ew, out: out)
+    }
+
     
    func mul(by: ExpertWeights, out: VectorFloat) {
        let weightBuckets = by.buckets
@@ -63,13 +58,9 @@ class BucketMulFast {
        
        assert(numBuckets % 4 == 0)
 
-       //let groups = 32
-//       dispatch.zero()
-//       tmpMulVec.zero()
        gpu.deploy("bucketMulFast", buffers: [weightBuckets, dispatch, tmpMulVec, dispatch.size],
                                ints: [weightBuckets.cols, mulGroups],
                                threadCount: [weightBuckets.cols, mulGroups])
-       
        
        let simdSize = 32
        gpu.deploy("bucketIntegrate", buffers: [tmpMulVec, out],
